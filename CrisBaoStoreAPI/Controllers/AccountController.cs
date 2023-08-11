0ae039a1bdc2046f7885edf501646bf7b226b161
CrisMixi <mobile02022002@gmail.com>
2023-08-11 16:33:41 +0700

    09 | 004 | Add Account Controller

---
+++ CrisBaoStoreAPI/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using CrisBaoStoreAPI.DTOs;
+using CrisBaoStoreAPI.Entites;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrisBaoStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login (LoginDto loginDto)
+        {
+            var user = await _userManager.FindByNameAsync(loginDto.UserName);
+            if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password)) 
+              {
+                Unauthorized();
+              }
+            return user;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterDto registerDto)
+        {
+            var user = new User { UserName = registerDto.UserName, Email = registerDto.Email };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded) // if Error
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description); // show a list error
+                }
+
+                return ValidationProblem();
+            }
+
+            await _userManager.AddToRoleAsync(user, "Member");
+            return StatusCode(201);
+        }
+        
+    }
+}
