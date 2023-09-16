abc8be3050790cdab6178258c7e1947fb7402f40
Cris <mobile02022002@gmail.com>
2023-09-16 08:35:24 +0700

    10 | 17 | 9 Updating and deleting images

--- CrisBaoStoreAPI/Controllers/ProductController.cs
+++ CrisBaoStoreAPI/Controllers/ProductController.cs
@@ -64,7 +64,7 @@ namespace CrisBaoStoreAPI.Controllers
 
         [Authorize(Roles = "Admin")]
         [HttpPost]
-        public async Task<ActionResult<Product>> CreateProduct([FromForm]CreateProductDto productDto) // Form là chuyển thành nhập dữ liệu theo form
+        public async Task<ActionResult<Product>> CreateProduct([FromForm]CreateProductDto productDto) // FormForm là chuyển thành nhập dữ liệu theo form
         {
             var product = _mapper.Map<Product>(productDto);
              
@@ -89,7 +89,7 @@ namespace CrisBaoStoreAPI.Controllers
 
         [Authorize(Roles = "Admin")]
         [HttpPut]
-        public async Task<ActionResult> UpdateProduct(UpdateProductDto productDto)
+        public async Task<ActionResult<Product>> UpdateProduct([FromForm]UpdateProductDto productDto)
         {
             var product = await _context.Products.FindAsync(productDto.Id);
 
@@ -97,9 +97,22 @@ namespace CrisBaoStoreAPI.Controllers
 
             _mapper.Map(productDto, product);
 
+            if (productDto.File != null)
+            {
+                var imageResult = await _imageService.AddImageAsync(productDto.File);
+
+                if (imageResult.Error != null) return BadRequest(new ProblemDetails { Title = imageResult.Error.Message });
+
+                if(!string.IsNullOrEmpty(product.PublicId))  
+                    await _imageService.DeleteImageAsync(product.PublicId); 
+
+                product.PictureUrl = imageResult.SecureUrl.ToString();
+                product.PublicId= imageResult.PublicId;
+            }
+
             var result = await _context.SaveChangesAsync() > 0;
             
-            if (result) return NoContent();
+            if (result) return Ok(product);
 
             return BadRequest(new ProblemDetails { Title = "Problem updating product"});
         }
